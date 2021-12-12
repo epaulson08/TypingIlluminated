0efde4f06dbd6286fa6cefd09c31abdb76ddaca0
Eric Paulson <epaulson08@icloud.com>
2021-12-12 15:26:43 -0700

    fix end-of-line return character issue; add styling

--- typing-illuminated.js
+++ typing-illuminated.js
@@ -1,29 +1,7 @@
 $(document).ready(() => {
   const title = "The Second Coming";
   const author = "William Butler Yeats";
-  let cont = `Turning and turning in the widening gyre
-  The falcon cannot hear the falconer;
-  Things fall apart; the centre cannot hold;
-  Mere anarchy is loosed upon the world,
-  The blood-dimmed tide is loosed, and everywhere
-  The ceremony of innocence is drowned;
-  The best lack all conviction, while the worst
-  Are full of passionate intensity.
-
-  Surely some revelation is at hand;
-  Surely the Second Coming is at hand.
-  The Second Coming! Hardly are those words out
-  When a vast image out of Spiritus Mundi
-  Troubles my sight: somewhere in sands of the desert
-  A shape with lion body and the head of a man,
-  A gaze blank and pitiless as the sun,
-  Is moving its slow thighs, while all about it
-  Reel shadows of the indignant desert birds.
-  The darkness drops again; but now I know
-  That twenty centuries of stony sleep
-  Were vexed to nightmare by a rocking cradle,
-  And what rough beast, its hour come round at last,
-  Slouches towards Bethlehem to be born?`;
+  // var cont is defined in separate script
 
   function spanify(str) {
     let toReturn = [...str];
@@ -47,28 +25,31 @@ $(document).ready(() => {
   // back from array to string
   toPresent = toPresent.join("");
 
-  // add <br />'s for newlines
-  toPresent = toPresent.replace(/\n/g, "<br />");
+  // add <br />'s for newlines;
+  // 23CE is return character to cue user
+  toPresent = toPresent.replace(/\n/g, "&#x23CE;<br />");
   $("#cont").html(toPresent);
 
   // create highlighter and initialize to first character
   let $current = $("#cont > span:first-child");
-  $current.css("background-color", "yellow");
+  // $current.css("background-color", "yellow");
+  $current.addClass("highlighted");
 
   // listen for keydowns
   let offset = 0;
   $(document).on("keydown", (e) => {
     console.log("typed: " + e.key);
-    console.log("expected: " + e.key);
+    console.log("expected: " + cont[offset]);
     // user typed correct key
     if (
       e.key === cont[offset] ||
       (cont[offset] === "\n" && e.key === "Enter")
     ) {
       offset++;
-      $current.css("background-color", "white");
+      $current.removeClass("highlighted");
       $current = $current.next("span");
-      $current.css("background-color", "yellow");
+      $current.addClass("highlighted");
+      // $current.css("background-color", "yellow");
     }
 
     // user typed incorrect key
