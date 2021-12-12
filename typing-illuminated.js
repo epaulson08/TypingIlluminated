17fe8ec5b63fc6307d6487da971d825d0e3cdaae
Eric Paulson <epaulson08@icloud.com>
2021-12-12 14:19:58 -0700

    replace single-line content with multi-line poem

--- typing-illuminated.js
+++ typing-illuminated.js
@@ -1,34 +1,29 @@
 $(document).ready(() => {
   const title = "The Second Coming";
   const author = "William Butler Yeats";
-  //   let cont = `Turning and turning in the widening gyre
-  // The falcon cannot hear the falconer;
-  // Things fall apart; the centre cannot hold;
-  // Mere anarchy is loosed upon the world,
-  // The blood-dimmed tide is loosed, and everywhere
-  // The ceremony of innocence is drowned;
-  // The best lack all conviction, while the worst
-  // Are full of passionate intensity.
+  let cont = `Turning and turning in the widening gyre
+  The falcon cannot hear the falconer;
+  Things fall apart; the centre cannot hold;
+  Mere anarchy is loosed upon the world,
+  The blood-dimmed tide is loosed, and everywhere
+  The ceremony of innocence is drowned;
+  The best lack all conviction, while the worst
+  Are full of passionate intensity.
 
-  // Surely some revelation is at hand;
-  // Surely the Second Coming is at hand.
-  // The Second Coming! Hardly are those words out
-  // When a vast image out of Spiritus Mundi
-  // Troubles my sight: somewhere in sands of the desert
-  // A shape with lion body and the head of a man,
-  // A gaze blank and pitiless as the sun,
-  // Is moving its slow thighs, while all about it
-  // Reel shadows of the indignant desert birds.
-  // The darkness drops again; but now I know
-  // That twenty centuries of stony sleep
-  // Were vexed to nightmare by a rocking cradle,
-  // And what rough beast, its hour come round at last,
-  // Slouches towards Bethlehem to be born?`;
-
-  // // add <br />'s to poem
-  // cont = cont.replace(/\n/g, "<br />");
-
-  let cont = "can i make it work with a single line?";
+  Surely some revelation is at hand;
+  Surely the Second Coming is at hand.
+  The Second Coming! Hardly are those words out
+  When a vast image out of Spiritus Mundi
+  Troubles my sight: somewhere in sands of the desert
+  A shape with lion body and the head of a man,
+  A gaze blank and pitiless as the sun,
+  Is moving its slow thighs, while all about it
+  Reel shadows of the indignant desert birds.
+  The darkness drops again; but now I know
+  That twenty centuries of stony sleep
+  Were vexed to nightmare by a rocking cradle,
+  And what rough beast, its hour come round at last,
+  Slouches towards Bethlehem to be born?`;
 
   function spanify(str) {
     let toReturn = [...str];
@@ -41,8 +36,21 @@ $(document).ready(() => {
   // generate initial content
   $("h1.title").text(title);
   $("h2.author").text(author);
-  // add span wrapper to every character of `cont`
-  $("#cont").html(spanify(cont));
+
+  // `cont` will remain the original string
+  // `toPresent` will be a copy that is changed to
+  // html
+  let toPresent = cont;
+
+  // add span wrapper to every character
+  toPresent = spanify(toPresent);
+  // back from array to string
+  toPresent = toPresent.join("");
+
+  // add <br />'s for newlines
+  toPresent = toPresent.replace(/\n/g, "<br />");
+  $("#cont").html(toPresent);
+
   // create highlighter and initialize to first character
   let $current = $("#cont > span:first-child");
   $current.css("background-color", "yellow");
@@ -50,9 +58,13 @@ $(document).ready(() => {
   // listen for keydowns
   let offset = 0;
   $(document).on("keydown", (e) => {
-    console.log(e.key);
+    console.log("typed: " + e.key);
+    console.log("expected: " + e.key);
     // user typed correct key
-    if (e.key === cont[offset]) {
+    if (
+      e.key === cont[offset] ||
+      (cont[offset] === "\n" && e.key === "Enter")
+    ) {
       offset++;
       $current.css("background-color", "white");
       $current = $current.next("span");
