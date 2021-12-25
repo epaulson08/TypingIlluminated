8aa1d5867ea3a2834914d2f66adafc85600302d0
Eric Paulson <epaulson08@icloud.com>
2021-12-24 22:05:10 -0700

    refactor: add helper functions

--- typing-illuminated.js
+++ typing-illuminated.js
@@ -1,25 +1,15 @@
 $(document).ready(() => {
   /* Declarations */
+  let state = {};
 
-  // character highlighter
-  let $current;
-
-  // initialize content text
-  const content = secondComing;
-  const cont = content.text;
-
-  // offset from start of `cont`. will increment when
-  // user types a correct key
+  // offset from start of `cont`
+  // state.offset = 0;
   let offset = 0;
 
   // will show a return symbol once the user types
   // to the end of a line, then hide it again when they
   // hit enter
   let showingReturn = false;
-
-  // meta-characters to ignore when determining if
-  // a correct key was pressed
-  const metas = ["Meta", "Shift", "Tab", "Alt", "CapsLock", "ArrowUp", "ArrowRight", "ArrowLeft", "ArrowDown", "Insert", "PageUp", "PageDown", "Home", "End", "Insert", "NumLock", "Escape"];
 
   // flag to track if user has not yet corrected
   // an incorrectly typed key
@@ -28,29 +18,34 @@ $(document).ready(() => {
   // store the offset of the incorrectly typed key
   let mistakeOffset = 0;
 
-  // make HTML from content
-  setUpPage(content);
+  // make HTML from content; $current is a span
+  // element wrapping the first typeable character
+  let $current = setUpPage(secondComing);
+  const cont = secondComing.text;
 
   /* Business Logic */
-  // TODO: handle "Delete" key
+  // TODOs: 
+  // Handle "Delete" key
   // Handle backspace from first character (highlighter disappears)
+  // Handle behavior on last character (e.g. when typed incorrectly)
+  // Fix backspace after return character behavior
 
   $(document).on("keydown", (e) => {
+    let key = e.key;
+
     // prevent browser default of Space causing down-scroll,
     // but do not lock up other metacharacters
-    if (e.code === "Space") {
+    if (key === " ") {
       e.preventDefault();
     }
 
-    const typedCorrect =
-      e.key === cont[offset] || (cont[offset] === "\n" && e.key === "Enter");
-
-    const backspace = e.key === "Backspace";
-
-    if (metas.includes(e.key)) {
+    if (_isMetaCharacter(key)) {
       // ignore meta-characters
       return;
-    } else if (backspace) {
+    } else if (_isLastCharacter(cont, offset)) {
+      // last character of text plus empty buffer characters
+      // TODO
+    } else if (_isBackspace(key)) {
       offset--;
       if (afterWrong) {
         $current.removeClass("after-wrong");
@@ -62,7 +57,7 @@ $(document).ready(() => {
         }
       } else {
         $current.removeClass("highlighted");
-        $current.removeClass("right")
+        $current.removeClass("right");
         $current = $current.prev("span");
         $current.removeClass("right");
         $current.addClass("highlighted");
@@ -71,7 +66,7 @@ $(document).ready(() => {
       offset++;
       $current = $current.next("span");
       $current.addClass("after-wrong");
-    } else if (typedCorrect) {
+    } else if (_typedCorrect(key, cont, offset)) {
       // for newlines, add return character to cue user to hit enter;
       // remove the symbol when they type the next character
       if (showingReturn) {
@@ -83,7 +78,13 @@ $(document).ready(() => {
         $current.next().html("<span>&#x23CE;<br /></span>");
         showingReturn = true;
       }
-      // user typed correct character
+      if (offset === cont.length - 1) {
+        $("#complete").text("Finished!");
+        $current.removeClass("highlighted");
+        $current.addClass("right");
+        _animate();
+        return;
+      }
       offset++;
       $current.removeClass("highlighted");
       $current.addClass("right");
@@ -99,38 +100,87 @@ $(document).ready(() => {
       offset++;
     }
   });
-
-  function setUpPage(content) {
-
-    function spanify(str) {
-      let toReturn = [...str];
-      for (let i = 0; i < toReturn.length; i++) {
-        toReturn.splice(i, 1, `<span>${toReturn[i]}</span>`);
-      }
-      return toReturn;
-    }
-
-    // generate initial content
-    $("h1.title").text(content.title);
-    $("h2.author").text(content.author);
-
-    // `content.text` will remain the original string
-    // `toPresent` will be a copy that is changed to
-    // html
-    let toPresent = content.text;
-
-    // add span wrapper to every character
-    toPresent = spanify(toPresent);
-
-    // change back from array to string
-    toPresent = toPresent.join("");
-
-    // add <br />'s for newlines
-    toPresent = toPresent.replace(/\n/g, "<br />");
-    $("#cont").html(toPresent);
-
-    // initialize highlighter
-    $current = $("#cont > span:first-child");
-    $current.addClass("highlighted");
-  }
 });
+
+// Presentation logic
+function setUpPage(content) {
+  $("h1.title").text(content.title);
+  $("h2.author").text(content.author);
+
+  // `content.text` will remain the original string
+  // `toPresent` will be a copy that is changed to
+  // html
+  let toPresent = content.text;
+
+  // add span wrapper to every character
+  toPresent = _spanify(toPresent);
+
+  // add <br />'s for newlines
+  toPresent = toPresent.replace(/\n/g, "<br />");
+  $("#cont").html(toPresent);
+
+  // initialize highlighter
+  let $firstTypeableCharacter = $("#cont > span:first-child");
+  $firstTypeableCharacter.addClass("highlighted");
+
+  return $firstTypeableCharacter;
+}
+
+function _markCorrect($element) {}
+
+function _markIncorrect($element) {}
+
+function _markHighlighted($element) {}
+
+function _markAfterIncorrect($element) {}
+
+function _spanify(str) {
+  let toReturn = [...str];
+  for (let i = 0; i < toReturn.length; i++) {
+    toReturn.splice(i, 1, `<span>${toReturn[i]}</span>`);
+  }
+  // change back from array to string
+  return toReturn.join("");
+}
+
+function _animate() {
+  console.log("This will do something soon");
+}
+
+// Business logic
+
+function _typedCorrect(key, cont, offset) {
+  return key === cont[offset] || (cont[offset] === "\n" && key === "Enter");
+}
+
+function _isBackspace(key) {
+  return key === "Backspace";
+}
+
+function _isLastCharacter(cont, offset) {
+  return offset === cont.length - 1;
+}
+
+function _isMetaCharacter(key) {
+  // meta-characters to ignore
+  const metas = [
+    "Meta",
+    "Shift",
+    "Tab",
+    "Alt",
+    "CapsLock",
+    "ArrowUp",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowDown",
+    "Insert",
+    "PageUp",
+    "PageDown",
+    "Home",
+    "End",
+    "Insert",
+    "NumLock",
+    "Escape",
+  ];
+  return metas.includes(key);
+}
